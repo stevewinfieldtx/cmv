b1c49eadb0f035905e1ba0f1befc7fc924bd06f6
Steve Winfield <stevewinfieldtx@gmail.com>
2025-06-30 23:35:46 -0600

    Add working /create JSON endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -9,6 +9,17 @@ def index():
 @app.route('/healthz')
 def healthz():
     return "ok", 200
+
+@app.route('/create', methods=['POST'])
+def create():
+    data = request.get_json()
+    print("Received from frontend:", data)
+    result = {
+        "status": "success",
+        "message": "Your video is being created!",
+        "echo": data
+    }
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run()
